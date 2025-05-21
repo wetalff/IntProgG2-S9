1504183dbc3e9ae9e17f07f1ed2f5849158ec93b
wetalff <alfredito.osejo@gmail.com>
2025-05-20 19:37:51 -0600

    Actividades de la semana 9

---
+++ Sesion 2/Act7.py
@@ -0,0 +1,14 @@
+#Encontrar el máximo y el mínimo
+#✓ A partir de una lista de números reales, encuentra el mayor y el menor
+#valor.
+
+
+Numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+mayor = max(Numeros)
+menor = min(Numeros)
+
+print("Lista:")
+print(Numeros)
+print("Mayor:", mayor)
+print("Menor:", menor)
